e19bf1550a59deb41254f9bc821e9506197a1761
xdd <45672942@qq.com>
2023-01-08 20:24:32 +0800

    导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom'
+import React, {useState} from 'react';
+import logoImg from '../src/assets/logo.svg'
+import {Outlet} from 'react-router-dom'
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
@@ -7,33 +8,48 @@ import {
     UserOutlined,
     VideoCameraOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
+import {Button, Layout, Menu, theme} from 'antd';
 import './assets/base.less'
-const { Header, Sider, Content, Footer } = Layout;
+
+const {Header, Sider, Content, Footer} = Layout;
 export default function App() {
+    const [collapsed, setCollapsed] = useState(false);
     return (
         <>
-            <Layout>
-                <header>
-                    Header
-                </header>
-                <Layout>
-                    <Sider>
-                        Sider
+            <Layout id='app'>
+                <Layout className='nav_sider'>
+                    <Sider trigger={null} collapsible collapsed={collapsed}>
+                        <h1 style={{ color: '#FFF', textAlign: 'center'}}>Dashboard</h1>
+                        <Menu
+                            theme="dark"
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            className='nav_menu'
+                            items={[
+                                {
+                                    key: '1',
+                                    icon: <UserOutlined/>,
+                                    label: 'nav 1',
+                                },
+                                {
+                                    key: '2',
+                                    icon: <VideoCameraOutlined/>,
+                                    label: 'nav 2',
+                                },
+                                {
+                                    key: '3',
+                                    icon: <UploadOutlined/>,
+                                    label: 'nav 3',
+                                },
+                            ]}
+                        />
                     </Sider>
                 </Layout>
-                <Content className='home-content'>
-                    11111111111
-                    {/*<div>*/}
-                    {/*    <Outlet/>*/}
-                    {/*</div>*/}
-                </Content>
-                <footer style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <footer>
                     &copy;2023 By 小薛
                     备案信息：陕ICP备2022014014号
                 </footer>
             </Layout>
-
         </>
     );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
+import imgLogo from '../../assets/logo.svg'
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import {Button, Layout, Menu, theme} from 'antd';
+
+const {Header, Sider, Content} = Layout;
 
 export default function Home() {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: {colorBgContainer},
+    } = theme.useToken();
+
     return (
-        <div>
-            Home
-        </div>
+        <Layout style={{ height: '100vh' }}>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <h1 style={{ color: "#FFF", textAlign: 'center'}}>Dashboard</h1>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined/>,
+                            label: 'nav 1',
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined/>,
+                            label: 'nav 2',
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined/>,
+                            label: 'nav 3',
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header>
+                    <Button onClick={() => setCollapsed(!collapsed)}>
+                        伸缩
+                    </Button>
+                    <Button>
+                        asdasd
+                    </Button>
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                    }}
+                >
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
     );
 };
 
