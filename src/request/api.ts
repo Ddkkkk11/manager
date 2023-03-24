4bf25e5817a04a29bb3903cfd704146776ca98db
xdd <45672942@qq.com>
2023-03-25 05:56:24 +0800

    type(api.ts): 增加评论的增删改查方法，添加params类型

--- src/request/api.ts
+++ src/request/api.ts
@@ -22,6 +22,35 @@ export const fetchMenu = (): Promise<any> => {
     return basicService.get('/menu')
 }
 //获取学生数据
-export const fetchStudent = (params: { student_name: string }): Promise<any> => {
+interface IFetchStudent {
+    student_name: string
+}
+export const fetchStudent = (params: IFetchStudent): Promise<any> => {
     return basicService.get(`/student?student_name=${params.student_name}`);
+}
+//获取管理员公告
+export const fetchAnnouncement = (): Promise<any> => {
+    return basicService.get("/announcement")
+}
+interface IFetchAnnouncement {
+    content: string
+}
+//添加公告
+export const addAnnouncement = (params: IFetchAnnouncement): Promise<any> => {
+    return basicService.post("/announcement", params);
+}
+
+//删除公告
+export const deleteAnnouncement = (id: number): Promise<any> => {
+    return basicService.delete(`/announcement/${id}`,)
+}
+
+//编辑公告
+export const editAnnouncement = (id: number, params: IFetchAnnouncement) => {
+    return basicService.patch(`/announcement/${id}`, params);
+}
+//查询公告
+
+export const searchAnnouncement = (params: IFetchAnnouncement): Promise<any> => {
+    return basicService.get(`/announcement`, { params });
 }
