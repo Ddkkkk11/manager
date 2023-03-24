70e7ba9395e5e0134ed9e5cb6405850c55c49034
xdd <45672942@qq.com>
2023-03-24 18:53:41 +0800

    fix(index.ts): 删除无用样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 // @ts-ignore
 import ReactDOM from 'react-dom/client';
-// import 'antd/dist/reset.css';
 import Router from './router';
-import App from "./App"
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
