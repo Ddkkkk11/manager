09a9728ee2d5bea8e8dfa4acce8955276d53d6a1
xdd <45672942@qq.com>
2023-05-18 04:45:16 +0800

    增加room和seat路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,11 +4,12 @@ import App from "../App";
 import Students from "../pages/Students";
 import Login from "../pages/Login";
 import Dashboard from "../containers/dashboard";
-import Stream from "../containers/stream";
 import Announcement from "../pages/Announcement";
 import Register from "../pages/Register";
 import Comment from "../pages/Comment";
 import Reply from "../pages/Reply";
+import Room from '../pages/Room';
+import Seat from '../pages/Seat';
 
 
 export default function BaseRouter() {
@@ -19,7 +20,8 @@ export default function BaseRouter() {
                     <Route path="/" element={<Navigate to="/login"/>}></Route>
                     <Route path='/dashboard' element={<Dashboard/>}></Route>
                     <Route path='/person' element={<Students/>}></Route>
-                    <Route path='/seat' element={<Stream/>}></Route>
+                    <Route path='/room' element={<Room/>}></Route>
+                    <Route path='/room/seat/:id' element={<Seat/>}></Route>
                     <Route path='/announcement' element={<Announcement/>}></Route>
                     <Route path='/comment' element={<Comment/>}></Route>
                     <Route path='/reply' element={<Reply/>}></Route>
