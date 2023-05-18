52b1f0d35e02c0becf7b8df853067dc48045487c
xdd <45672942@qq.com>
2023-05-18 17:23:46 +0800

    fix(seat):增加座位列表

--- src/pages/Seat/index.tsx
+++ src/pages/Seat/index.tsx
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Button, Input, Table } from 'antd';
-import { getSeat, getSeatByNumber } from '../../request/api';
+import { Button, Input, message, Modal, Table } from 'antd';
+import { addSeat, deleteSeat, getSeat, getSeatByNumber } from '../../request/api';
 import { SearchOutlined } from '@ant-design/icons';
+import './style.less'
 export default function Seat() {
     const { id } = useParams();
     const [data, setData] = useState([]);
     const [searchVal, setSearchVal] = useState('');
+
     useEffect(() => {
         getSeat(id).then(res => {
             const data = res.data;
             setData(data);
         })
     }, []);
+    const onDeleteSeat = (seatId: number) => {
+        Modal.confirm({
+            title: '释放座位',
+            content: `确定要释放这个ID为${seatId}的座位吗`,
+            onOk: () => {
+                deleteSeat(seatId).then(res => {
+                    message.success('释放成功');
+                    getSeat(id).then(res => {
+                        const data = res.data;
+                        setData(data);
+                    })
+
+                }).catch(err => {
+                    message.error('释放失败');
+                })
+            }
+        })
+    }
     const columns = [
         {
             title: '座位ID',
@@ -44,7 +64,7 @@ export default function Seat() {
             dataIndex: 'action',
             key: 'action',
             render: (action: any, record: any) => {
-                return record.status===1 ? <Button type="primary"  danger>释放</Button> : '--'
+                return record.status===1 ? <Button type="primary"  danger onClick={() => onDeleteSeat(record.id)}>释放</Button> : '--'
             }
         }
     ]
@@ -56,12 +76,40 @@ export default function Seat() {
             setData(data);
             //TODO 接口问题传空值报错
         })
+    }
 
-
+    //点击添加座位
+    const onHandle = (roomId: any) => {
+        const params = {
+            roomId: Number(roomId),
+            status: 0,
+            seatNumber: Number(roomId)
+        }
+        Modal.confirm({
+            title: `添加`,
+            content: '是否增加一个新的座位',
+            onOk: () => {
+                addSeat(params).then(res => {
+                    message.success('添加成功');
+                    getSeat(id).then(res => {
+                        const data = res.data;
+                        setData(data);
+                    })
+                }).catch(err => {
+                    message.error('添加失败');
+                })
+            }
+        });
     }
     return (
-        <section>
-            <div className='search-area'>
+        <section className='seat-area'>
+            <div className='seat-search-area'>
+                <Button
+                    type='primary'
+                    onClick={() => onHandle(id)}
+                >添加座位
+                </Button>
+
                 <div>
                     <Input
                         placeholder='座位编号'
